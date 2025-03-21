ac3b44a813d084cde3f29f15d00cd8ddbde6462c
Andrew Theaker <andrew@theaker.name>
2025-03-22 07:17:32 +1100

    Add verbose output

--- install/pre_install_vm.sh
+++ install/pre_install_vm.sh
@@ -8,6 +8,7 @@ swapsize = 4GB
 # NixOS preinstall script for a VM with UEFI
 # Basic disk partitioning
 
+echo "Partitioning disk..."
 parted /dev/sda -- mklabel gpt
 parted /dev/sda -- mkpart root ext4 512MB -$swapsize
 parted /dev/sda -- mkpart swap linux-swap -$swapsize 100%
@@ -15,12 +16,16 @@ parted /dev/sda -- mkpart ESP fat32 1MB 512MB
 parted /dev/sda -- set 3 esp on
 
 # format partiions
+echo "Formatting partitions..."
 mkfs.ext4 -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 mkfs.fat -F 32 -n boot /dev/sda3
 
 # Mount the system
+echo "Mounting filesystem..."
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 swapon /dev/sda2
+
+echo "Disk setup complete. Move configuration.nix file into /mnt/etc/nixos/ and run nixos-install"

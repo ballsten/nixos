93b45b8813052c935b94ccb2a0c17d89d57eabd5
Andrew Theaker <andrew@theaker.name>
2025-03-22 16:54:29 +1100

    Update install script to copy config and run install

--- install/install_bootstrap.sh
+++ install/install_bootstrap.sh
@@ -3,29 +3,31 @@
 #! nix-shell -p bash parted e2fsprogs util-linux dosfstools
 
 # Variables
-swapsize=4GB
+SWAP_SIZE=4GB
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# NixOS preinstall script for a VM with UEFI
-# Basic disk partitioning
+# bootstrap NixOS script for a VM with UEFI
 
 echo "Partitioning disk..."
 parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart root ext4 512MB -$swapsize
-parted /dev/sda -- mkpart swap linux-swap -$swapsize 100%
+parted /dev/sda -- mkpart root ext4 512MB -$SWAP_SIZE
+parted /dev/sda -- mkpart swap linux-swap -$SWAP_SIZE 100%
 parted /dev/sda -- mkpart ESP fat32 1MB 512MB
 parted /dev/sda -- set 3 esp on
 
-# format partiions
 echo "Formatting partitions..."
 mkfs.ext4 -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 mkfs.fat -F 32 -n boot /dev/sda3
 
-# Mount the system
 echo "Mounting filesystem..."
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot
 mount -o umask=077 /dev/disk/by-label/boot /mnt/boot
 swapon /dev/sda2
 
-echo "Disk setup complete. Move configuration.nix file into /mnt/etc/nixos/ and run nixos-install"
+echo "Copying bootstrap configuration..."
+cp $SCRIPT_DIR/../environments/bootstrap/*.nix /mnt/etc/nixos/ 
+
+echo "Installing NixOS..."
+nixos-install --no-root-passwd

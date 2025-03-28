58b5b142152265bb58d2e3db94d44c2860af925b
Andrew Theaker <andrew@theaker.name>
2025-03-28 02:47:35 +0000

    Add WSL to config

--- flake.nix
+++ flake.nix
@@ -7,10 +7,13 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
-
+  outputs = { nixpkgs, home-manager, nixos-wsl, ... }@inputs: {
     nixosConfigurations = {
       dev-vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -32,6 +35,29 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/surface-laptop/configuration.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.ballsten = import ./home;
+	  }
+        ];
+      };
+
+      wsl = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          nixos-wsl.nixosModules.wsl
+          ./hosts/wsl/configuration.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.ballsten = import ./home;
+	  }
         ];
       };
     };
